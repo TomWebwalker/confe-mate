1063adb52db29c3f7bd3028946d89b89f0f7a857
Tomasz Flis <tomaszwwl@gmail.com>
2025-10-14 21:31:54 +0200

    feat: implement global styles and integrate chat component into app

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,13 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Chat } from './components/chat/chat';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, Chat],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
-  protected readonly title = signal('confe-mate');
+  protected readonly title = signal('ConfeMate');
 }
